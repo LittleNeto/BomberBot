c6ce6c2532763ba30ba1563d8f21c1ced8c37f4f
José Agra Neto <agrajoseneto@gmail.com>
2025-04-30 16:53:01 -0300

    código genérico para servir de base para os contribuidores do projeto

---
+++ src/principal/Main.java
@@ -0,0 +1,10 @@
+package principal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("código principal básico para rodar o jogo (ainda em desenvolvimento)");
+
+	}
+
+}
