2a79e1a8cfd44e6ace27c215c297f2f4ea10879c
mateuserikNA <mateuserikjd87y@gmail.com>
2025-05-06 23:28:25 -0300

    Update Main.java
    main atualizado com a parte inicial da interface gráfica

--- src/principal/Main.java
+++ src/principal/Main.java
@@ -1,10 +1,30 @@
 package principal;
 
+/**
+ *
+ * @author Mateus
+ */
+
+import javax.swing.JFrame;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("código principal básico para rodar o jogo (ainda em desenvolvimento)");
-
-	}
-
+    public static void main(String[] args) {
+        JFrame janela = new JFrame();
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        janela.setResizable(false); //para que não seja possível modificar o tamanho da janela
+        janela.setTitle("BomberBot");
+        
+        GamePanel gamePanel = new GamePanel();
+        janela.add(gamePanel);
+        
+        janela.pack(); //redimensiona a janela com base nos seus componentes(no caso, o GamePanel)
+        
+        janela.setLocationRelativeTo(null); //coloca a janela no centro da tela
+        janela.setVisible(true);
+        
+        gamePanel.StartGameThread();
+        
+    }
+    
 }
