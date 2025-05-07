d54f4e02604ef7044f77c0dea2f29549effcc6fe
mateuserikNA <mateuserikjd87y@gmail.com>
2025-05-06 23:31:03 -0300

    Add files via upload
    criação da classe GamePanel (a tela onde o jogo é exibido) e ManipuladorTeclado (que faz a lógica do movimento do player com base na tecla pressionada)

---
+++ src/principal/GamePanel.java
@@ -0,0 +1,108 @@
+package principal;
+
+/**
+ *
+ * @author Mateu
+ */
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+public class GamePanel extends JPanel implements Runnable{
+    //configurações da tela
+    final int tileSizeOriginal = 32; //tamanho padrão de cada "bloco" da tela
+    final int escala = 2; //reescala a tela para que 32X32 não fique tão pequeno
+    
+    final int tileSize = tileSizeOriginal * escala; //tile de 64X64
+    final int maxScreenCol = 20;
+    final int maxScreenLin = 15;
+    final int screenWidth = tileSize * maxScreenCol; //1280 pixels de largura
+    final int screenHeight = tileSize * maxScreenLin; //960 pixels de altura
+    
+    //FPS
+    int fps = 60;
+    
+    ManipuladorTeclado keyH = new ManipuladorTeclado();
+    Thread gameThread; //implementado para ajudar a atualizar a tela durante o decorrer do jogo
+    
+    //configurando as posições do jogador (MODIFICA DEPOIS PARA POO)
+    int playerX = 100;
+    int playerY = 100;
+    int playerVelocidade = 4;
+    
+    public GamePanel() {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight)); //define a dimensão da tela
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true); //melhora a renderização do jogo
+        this.addKeyListener(keyH); //o gamePanel vai reconhecer o input das teclas
+        this.setFocusable(true);
+    }
+
+    public void StartGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start(); //chama o método run
+    }
+
+    @Override
+    public void run() { //game loop
+        
+        double intervaloDesenho = 1000000000 / fps;
+        double delta = 0;
+        long tempoAntes = System.nanoTime();
+        long tempoAtual;
+        long timer = 0;
+        int drawCount = 0;
+        
+        while (gameThread != null) {
+            
+            tempoAtual = System.nanoTime();
+            
+            delta += (tempoAtual - tempoAntes) / intervaloDesenho;
+            timer += (tempoAtual - tempoAntes);
+            tempoAntes = tempoAtual;
+            
+            if (delta >= 1) {
+                // 1 - UPDATE: atualizar as informações (como posição do jogador)
+                update();
+                // 2 - DESENHAR: desenhar a tela com as informações atualizadas
+                repaint(); // chama o método paintComponent
+                delta--;
+                drawCount++;
+            }
+            
+            if (timer >= 1000000000) {
+                System.out.println("Fps: " + drawCount);
+                drawCount = 0;
+                timer = 0;
+            }
+        }
+    }
+    
+    public void update() {
+        if (keyH.cimaPress == true) {
+            playerY -= playerVelocidade; //através da velocidade determinamos quantos pixels o player se move
+        } else if (keyH.baixoPress == true) {
+            playerY += playerVelocidade;
+        } else if (keyH.esqPress == true) {
+            playerX -= playerVelocidade;
+        } else if (keyH.dirPress == true) {
+            playerX += playerVelocidade;
+        }
+    }
+    
+    public void paintComponent(Graphics g) { //está sobrescrevendo um método que já existe em java
+        
+        super.paintComponent(g);
+        
+        Graphics2D g2 = (Graphics2D)g; //tem mais funções que o Graphics
+        
+        g2.setColor(Color.white);
+        
+        g2.fillRect(playerX, playerY, tileSize, tileSize);  //TESTE
+        
+        g2.dispose(); //libera memória do que não está sendo mais usado
+    }
+}
---
+++ src/principal/ManipuladorTeclado.java
@@ -0,0 +1,56 @@
+package principal;
+
+/**
+ *
+ * @author Mateu
+ */
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class ManipuladorTeclado implements KeyListener { 
+//implementa uma interface para "ler" as teclas pressionadas
+    
+    public boolean cimaPress, baixoPress, esqPress, dirPress;
+    
+    @Override
+    public void keyTyped(KeyEvent e) {
+    } //não será utilizado
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode(); //retorna o int associado ao código da tecla
+        
+        if (code == KeyEvent.VK_W) { //caso a tecla W seja pressionada
+            cimaPress = true;
+        }
+        if (code == KeyEvent.VK_A) { //caso a tecla W seja pressionada
+            esqPress = true;
+        }
+        if (code == KeyEvent.VK_S) { //caso a tecla W seja pressionada
+            baixoPress = true;
+        }
+        if (code == KeyEvent.VK_D) { //caso a tecla W seja pressionada
+            dirPress = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) { 
+        int code = e.getKeyCode(); //retorna o int associado ao código da tecla
+        
+        if (code == KeyEvent.VK_W) { //caso a tecla W seja pressionada
+            cimaPress = false;
+        }
+        if (code == KeyEvent.VK_A) { //caso a tecla W seja pressionada
+            esqPress = false;
+        }
+        if (code == KeyEvent.VK_S) { //caso a tecla W seja pressionada
+            baixoPress = false;
+        }
+        if (code == KeyEvent.VK_D) { //caso a tecla W seja pressionada
+            dirPress = false;
+        }
+    }
+    
+}
