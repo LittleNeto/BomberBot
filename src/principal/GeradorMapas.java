ec1522b34084f03be675227e6c277eadb6131381
José Agra Neto <agrajoseneto@gmail.com>
2025-05-17 19:01:24 -0300

    posições adjacentes ao jogador
    agora, as posições adjacentes ao jogador não podem ser blocos. isso vai facilitar a implementação da colisão

--- src/principal/GeradorMapas.java
+++ src/principal/GeradorMapas.java
@@ -1,10 +1,5 @@
 package principal;
 
-/**
- *
- * @author Neto
- */
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -12,22 +7,24 @@ import java.io.IOException;
 
 public class GeradorMapas {
 	
-	private static final int numLinhas = 10; //número de linhas do mapa
-	private static final int numColunas = 17; //número de colunas do mapa
-	private static final String caminho = "res/mapas/mapa01.txt"; //indica o nome do arquivo em que o mapa será guardado
+	private static final int numLinhas = 10;
+	private static final int numColunas = 17;
+	private static final String caminho = "res/mapas/mapa01.txt";
 	
 	public static void gerarMapa() {
 		
-		int[][] mapa = new int[numLinhas][numColunas]; //matriz para representar o mapa
+		int[][] mapa = new int[numLinhas][numColunas];
         
         for (int i = 0; i < numLinhas; i++) {
             for (int j = 0; j< numColunas; j++) {
-                if (i == 0 || i == numLinhas - 1 || j == 0 || j == numColunas - 1) { //condição para gerar os bolcos da borda do mapa
+                if (i == 0 || i == numLinhas - 1 || j == 0 || j == numColunas - 1) {
                     mapa[i][j] = 1;
-                } else if ((i % 2 == 0) && (j % 2 == 0)) { //condição para posicionar os blocos fixo no mapa
+                } else if ((i % 2 == 0) && (j % 2 == 0)) {
                     mapa[i][j] = 1;
+                } else if (i * j == 1 || i * j == 2) {
+                    mapa[i][j] = 0;
                 } else {
-                    mapa[i][j] = Math.random() > 0.7f ? 2 : 0; //escolhe aleatoriamente onde é bloco destrutível (30% de chance de escolha) e onde é só terra (70% de chance de escolha)
+                    mapa[i][j] = Math.random() > 0.7f ? 2 : 0;
                 }
             }    
         }
@@ -49,9 +46,9 @@ public class GeradorMapas {
             for (int i = 0; i < numLinhas; i++) {
                 for (int j = 0; j < numColunas; j++) {
                     writer.write(Integer.toString(mapa[i][j]));
-                    if (j != numColunas - 1) writer.write(" "); //dá espaçamento entre os elementos da linha
+                    if (j != numColunas - 1) writer.write(" ");
                 }
-                if (i != numLinhas - 1) writer.write("\n"); //para que os elementos não fiquem em uma única linha
+                if (i != numLinhas - 1) writer.write("\n");
             }
             writer.close();
             System.out.println("Arquivo gerado com sucesso em: " + caminho);
@@ -61,3 +58,4 @@ public class GeradorMapas {
     }
 	
 }
+
