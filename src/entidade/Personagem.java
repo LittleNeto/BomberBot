ffcedde5a06ca6c517ee23fc3bd13cd8e3c196ce
mateuserikNA <mateuserikjd87y@gmail.com>
2025-05-18 23:50:02 -0300

    Update perspectiva player

--- src/entidade/Personagem.java
+++ src/entidade/Personagem.java
@@ -7,7 +7,7 @@ import java.awt.image.BufferedImage;
  * @author Mateus
  */
 public class Personagem {
-    public int x, y;
+    public int mundoX, mundoY;
     public int velocidade;
     
     public BufferedImage cima1, cima2, baixo1, baixo2, esq1, esq2, dir1, dir2;
