0b2695adb5326a0fa28e2636cdbad1e4e8ae5e95
mateuserikNA <mateuserikjd87y@gmail.com>
2025-05-20 23:34:30 -0300

    Update Jogador.java

--- src/entidade/Jogador.java
+++ src/entidade/Jogador.java
@@ -1,6 +1,5 @@
 package entidade;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -15,24 +14,23 @@ import principal.ManipuladorTeclado;
 public class Jogador extends Personagem{
      GamePanel gp;
      ManipuladorTeclado keyH;
+     
      public final int telaX;
-     public final int telaY;
-
      
      public Jogador(GamePanel gp, ManipuladorTeclado keyH) {
          this.gp = gp;
          this.keyH = keyH;
          
-         telaX = gp.screenWidth / 2 - (gp.tileSize / 2); //para o jogador aparecer na metade da tela
-         telaY = gp.screenHeight / 2 - (gp.tileSize / 2);
+         telaX = gp.screenWidth / 2 - (gp.tileSize / 2);
          
          setDefaultValues();
          getImagemJogador();
      }
      
      public void setDefaultValues() {
-         mundoX = gp.tileSize * 1; //posição do jogador no começo do jogo
-         mundoY = gp.tileSize * 1;
+         x = gp.tileSize * 1;
+         mundoX = gp.tileSize * 1;
+         y = gp.tileSize * 1;
          velocidade = 3;
          direcao = "baixo"; //a string será usada em um switch para determinar a imagem correspondente
      }
@@ -61,15 +59,17 @@ public class Jogador extends Personagem{
     		
             if (keyH.cimaPress == true) {
             	direcao = "cima";
-            	mundoY -= velocidade; //através da velocidade determinamos quantos pixels o player se move
+                y -= velocidade; //através da velocidade determinamos quantos pixels o player se move
             } else if (keyH.baixoPress == true) {
             	direcao = "baixo";
-            	mundoY += velocidade;
+                y += velocidade;
             } else if (keyH.esqPress == true) {
             	direcao = "esquerda";
-            	mundoX -= velocidade;
+                x -= velocidade;
+                mundoX -= velocidade;
             } else if (keyH.dirPress == true) {
             	direcao = "direita";
+                x += velocidade;
             	mundoX += velocidade;
             }
             
@@ -87,47 +87,26 @@ public class Jogador extends Personagem{
     	
      }
      public void desenhar(Graphics2D g2) {
-//        g2.setColor(Color.white);
-//        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
-    	 
-    	 BufferedImage imagem = null;
-    	 
-    	 switch(direcao) {
-    	 case "cima":
-    		 if (spriteNum == 1) {
-    			 imagem = cima1;
-    		 }
-    		 if (spriteNum == 2) {
-    			 imagem = cima2;
-    		 }
-    		 break;
-    	 case "baixo":
-    		 if (spriteNum == 1) {
-    			 imagem = baixo1;
-    		 }
-    		 if (spriteNum == 2) {
-    			 imagem = baixo2;
-    		 }
-    		 break;
-    	 case "esquerda":
-    		 if (spriteNum == 1) {
-    			 imagem = esq1;
-    		 }
-    		 if (spriteNum == 2) {
-    			 imagem = esq2;
-    		 }
-    		 break;
-    	 case "direita":
-    		 if (spriteNum == 1) {
-    			 imagem = dir1;
-    		 }
-    		 if (spriteNum == 2) {
-    			 imagem = dir2;
-    		 }
-    		 break;			  
-    	 }
-    	 
-    	 g2.drawImage(imagem, telaX, telaY, gp.tileSize, gp.tileSize, null);
-    	 
-     }
+    	    BufferedImage imagem = null;
+
+    	    switch (direcao) {
+    	        case "cima": imagem = (spriteNum == 1) ? cima1 : cima2; break;
+    	        case "baixo": imagem = (spriteNum == 1) ? baixo1 : baixo2; break;
+    	        case "esquerda": imagem = (spriteNum == 1) ? esq1 : esq2; break;
+    	        case "direita": imagem = (spriteNum == 1) ? dir1 : dir2; break;
+    	    }
+
+    	    int drawX = gp.jogador.telaX;
+
+    	    // Bordas do mapa
+    	    if (mundoX < telaX) {
+    	        drawX = mundoX; // início do mapa
+    	    } else if (mundoX > gp.tileSize * gp.maxMundoCol - (gp.screenWidth - telaX)) {
+    	        drawX = mundoX - (gp.tileSize * gp.maxMundoCol - gp.screenWidth); // final do mapa
+    	    }
+
+    	    g2.drawImage(imagem, drawX, y, gp.tileSize, gp.tileSize, null);
+	}
+
+
 }
