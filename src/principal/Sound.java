af894befdc8145d44bb4337b03e4266027902fda
mateuserikNA <mateuserikjd87y@gmail.com>
2025-06-07 20:24:03 -0300

    Criando as músicas e o enum

---
+++ src/principal/Sound.java
@@ -0,0 +1,63 @@
+package principal;
+
+import java.net.URL;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Sound {
+	
+	Clip clip;
+	URL soundURL[] = new URL[30]; //uma lista dos 'paths' dos sons
+	private long posicaoAtual = 0;
+	
+	public Sound() {
+		
+		soundURL[0] = getClass().getResource("/musicas/musica1.wav");
+		soundURL[1] = getClass().getResource("/musicas/musica2.wav");
+		soundURL[2] = getClass().getResource("/musicas/musica3.wav");
+	}
+	
+	public void setFile(int i) {
+		
+		try {
+			AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
+			clip = AudioSystem.getClip();
+			clip.open(ais);
+		} catch(Exception e) {
+		}
+	}
+	
+	public void play() {
+		if (clip != null) { //para que não tente começar a música antes de carregá-la
+			clip.start(); 	
+		}
+	}
+	
+	public void loop() {
+		if (clip != null) { //para que não tente começar a música antes de carregá-la
+			clip.loop(clip.LOOP_CONTINUOUSLY); //para a música repetir após um tempo	
+		}
+	}
+	
+	public void parar() {
+		if (clip != null) { //para que não tente começar a música antes de carregá-la
+			clip.stop();
+		}
+	}
+	
+	public void pausar() {
+		if (clip != null && clip.isRunning()) {
+			posicaoAtual = clip.getMicrosecondPosition(); //pega o tempo que parou a música em microssegundos e salva antes de parar
+			clip.stop();
+		}
+	}
+	
+	public void retomar() {
+		if(clip != null) {
+			clip.setMicrosecondPosition(posicaoAtual); //carrega o tempo da música que havia sido salva
+			clip.start();
+		}
+	}
+}
