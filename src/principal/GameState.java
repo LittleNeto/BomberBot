839ca8f62e41670a292750bc8cf29f21837f3b64
mateuserikNA <mateuserikjd87y@gmail.com>
2025-06-07 20:27:11 -0300

    Criando as músicas e o enum

---
+++ src/principal/GameState.java
@@ -0,0 +1,18 @@
+package principal;
+
+//cada música tem seu próprio valor e o -1 indica que não tem
+public enum GameState {
+    TITULO(0),
+    PLAY(1),    
+    PAUSE(-1);  
+
+    private final int musicaIndex;
+
+    private GameState(int musicaIndex) {
+        this.musicaIndex = musicaIndex;
+    }
+
+    public int getMusicaIndex() {
+        return musicaIndex;
+    }
+}
