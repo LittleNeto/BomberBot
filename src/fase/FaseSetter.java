7f2dac2aa2695c05e8f16dc8cb21cb953ddbae4d
mateuserikNA <mateuserikjd87y@gmail.com>
2025-06-14 21:30:32 -0300

    Update FaseSetter

--- src/fase/FaseSetter.java
+++ src/fase/FaseSetter.java
@@ -1,7 +1,6 @@
 package fase;
 
 import principal.GamePanel;
-import objeto.OBJ_Porta;
 import recursos.GeradorMapa;
 import tile_Interativo.BlocoLixo;
 
@@ -9,13 +8,9 @@ public interface FaseSetter {
 	
 		void setInimigos(GamePanel gp);
 	
-	    default void setObject(GamePanel gp) {
-	    	gp.obj[0] = new OBJ_Porta(gp);
-	    	gp.obj[0].mundoX = gp.getTileSize() * 2;
-	    	gp.obj[0].mundoY = gp.getTileSize() * 1;
-	    }
+	    void setObject(GamePanel gp);
 	    
-	    default void setBlocoInterativo(GamePanel gp,GeradorMapa gMapa) {
+	    default void setBlocoInterativo(GamePanel gp, GeradorMapa gMapa) {
 	    	int i = 0;
 	        for (int lin = 0; lin < gMapa.getGrade().length; lin++) {
 	            for (int col = 0; col < gMapa.getGrade()[0].length; col++) {
